7941e0a88f9d7fde11d25ae1c93c9886d77a6397
ffiirree <ice_qi@163.com>
2023-04-21 13:52:18 +0800

    add comments

--- 01_remuxing/remuxing.cpp
+++ 01_remuxing/remuxing.cpp
@@ -14,6 +14,7 @@ int main(int argc, char* argv[])
     const char * in_filename = argv[1];
     const char * out_filename = argv[2];
 
+    // input
     AVFormatContext* decoder_fmt_ctx = nullptr;
     if (avformat_open_input(&decoder_fmt_ctx, in_filename, nullptr, nullptr) < 0) {
         fprintf(stderr, "failed to open the %s file.\n", in_filename);
@@ -27,15 +28,16 @@ int main(int argc, char* argv[])
 
     av_dump_format(decoder_fmt_ctx, 0, in_filename, 0);
 
-    //
     // output
-    //
     AVFormatContext * encoder_fmt_ctx = nullptr;
     if (avformat_alloc_output_context2(&encoder_fmt_ctx, nullptr, nullptr, out_filename) < 0) {
         fprintf(stderr, "failed to alloc output-context memory.\n");
         return -1;
     }
 
+    // map streams
+    //
+    // a media file always contains several streams, like video and audio streams.
     std::map<unsigned int, int> stream_mapping{};
     int stream_idx = 0;
     for (unsigned int i = 0; i < decoder_fmt_ctx->nb_streams; i++) {
@@ -61,6 +63,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    // open the output file
     if(!(encoder_fmt_ctx->oformat->flags & AVFMT_NOFILE)) {
         if (avio_open(&encoder_fmt_ctx->pb, out_filename, AVIO_FLAG_WRITE) < 0) {
             printf("can not open the output file : %s.\n", out_filename);
@@ -68,6 +71,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    // header
     if(avformat_write_header(encoder_fmt_ctx, nullptr) < 0) {
         fprintf(stderr, "can not write header to the output file.\n");
         return -1;
@@ -75,33 +79,49 @@ int main(int argc, char* argv[])
 
     av_dump_format(encoder_fmt_ctx, 0, out_filename, 1);
 
+    // copy streams
     AVPacket * packet = av_packet_alloc();
     int64_t frame_number = 0;
     while(av_read_frame(decoder_fmt_ctx, packet) >= 0) {
         if (stream_mapping[packet->stream_index] < 0) {
+            // the packet is reference-counted.
+            // the ffmpeg is a C library, we need unreference the buffer manually
             av_packet_unref(packet);
             continue;
         }
 
-        av_packet_rescale_ts(packet, decoder_fmt_ctx->streams[packet->stream_index]->time_base, encoder_fmt_ctx->streams[stream_mapping[packet->stream_index]]->time_base);
-        packet->stream_index = stream_mapping[packet->stream_index];
+        // convert the timestamps from the time base of input stream to the time base of output stream
+        //
+        // In simple terms, a time base is a rescaling of the unit second, like 1/100s, 1001/24000s, and
+        // each stream has its own time base. The timestamps of the packets are based on the time base 
+        // of their streams, timestamps of the same value have different meanings depending on the time unit.
+        //
+        // It is not necessary in this example since the time base of input and output streams is the same.
+        av_packet_rescale_ts(packet, 
+                             decoder_fmt_ctx->streams[packet->stream_index]->time_base, 
+                             encoder_fmt_ctx->streams[stream_mapping[packet->stream_index]]->time_base);
 
         printf(" -- %s] pts: %6ld, dts: %6ld, duration: %5ld, frame = %6ld\n",
                av_get_media_type_string(encoder_fmt_ctx->streams[packet->stream_index]->codecpar->codec_type),
                packet->pts, packet->dts, packet->duration, frame_number++);
 
+        packet->stream_index = stream_mapping[packet->stream_index];
+        // write the packet to the output file
+        // this function will take the owership of the packet, and packet will be blank
         if (av_interleaved_write_frame(encoder_fmt_ctx, packet) != 0) {
             fprintf(stderr, "failed to write frame.\n");
             return -1;
         }
-        av_packet_unref(packet);
     }
-    av_packet_free(&packet);
 
+    // write the trailer to the output file and close it
     av_write_trailer(encoder_fmt_ctx);
     if (encoder_fmt_ctx && !(encoder_fmt_ctx->oformat->flags & AVFMT_NOFILE))
         avio_closep(&encoder_fmt_ctx->pb);
 
+    // release the resources
+    // let's not think about the abnormal exit for simplicity
+    av_packet_free(&packet);
     avformat_close_input(&decoder_fmt_ctx);
     avformat_free_context(encoder_fmt_ctx);
 
